8389cbb9fb7ac559a586f97bce433befbde7d4d2
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-13 13:54:16 +0900

    Added Button component

---
+++ src/components/atoms/AddButton/index.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withSmartKnobs } from 'storybook-addon-smart-knobs';
+import { withNotes } from '@storybook/addon-notes';
+import { withInfo } from '@storybook/addon-info';
+import AddButton from '.';
+
+storiesOf('Atoms/AddButton', module)
+  .addDecorator((story, context) =>
+    withInfo(`
+      ~~~jsx
+      <AddButton>Text</AddButton>
+      ~~~
+    `)(story)(context)
+  )
+  .addDecorator(withSmartKnobs)
+  .addDecorator(withKnobs)
+  .addWithJSX('default', withNotes(``)(() => <AddButton>Text</AddButton>));
---
+++ src/components/atoms/AddButton/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import styled from 'styled';
+import Icon from 'components/atoms/Icon';
+
+const AddButton: React.SFC<{}> = ({ children }) => {
+  return (
+    <Wrapper>
+      <Icon icon="add" />
+      <span>{children}</span>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  background-color: ${props => props.theme.brandPrimary};
+  padding: ${props => props.theme.sizeBase};
+
+  svg {
+    width: 18px;
+    height: 18px;
+    display: block;
+    margin: 0 ${props => props.theme.sizeBaseIconKerning} 0 0;
+    > circle {
+      fill: ${props => props.theme.white} !important;
+    }
+    > line {
+      stroke: ${props => props.theme.brandPrimary} !important;
+    }
+  }
+
+  span {
+    display: block;
+    color: ${props => props.theme.white};
+    font-weight: bold;
+    line-height: 1em;
+  }
+`;
+
+export default AddButton;
