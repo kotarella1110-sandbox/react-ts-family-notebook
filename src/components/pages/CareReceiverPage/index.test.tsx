19e21aef1aeb3394fdc45ca5d112542bbf8df1d3
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-25 11:54:06 +0900

    Fixed CareReceiverPage component

--- src/components/pages/CareReceiverPage/index.test.tsx
+++ src/components/pages/CareReceiverPage/index.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import CareReceiverPage from '.';
 import AppTemplate from '../../templates/AppTemplate';
+import ModalTemplate from '../../templates/ModalTemplate';
 
 const setup = () => {
   const props = {};
@@ -14,6 +15,8 @@ const setup = () => {
 describe('CareReceiverPage', () => {
   it('コンポーネントがレンダリングされていること', () => {
     const { wrapper } = setup();
-    expect(wrapper.type()).toBe(AppTemplate);
+    expect(wrapper.type()).toBe('div');
+    expect(wrapper.find('AppTemplate').type()).toBe(AppTemplate);
+    expect(wrapper.find('ModalTemplate').type()).toBe(ModalTemplate);
   });
 });
--- src/components/pages/CareReceiverPage/index.tsx
+++ src/components/pages/CareReceiverPage/index.tsx
@@ -1,31 +1,66 @@
 import * as React from 'react';
 import AppTemplate from '../../templates/AppTemplate';
+import ModalTemplate from '../../templates/ModalTemplate';
 import AddButton from '../../atoms/AddButton';
 import Header from '../../organisms/Header';
 import CareReceiverList from '../../organisms/CareReceiverList';
 
 export interface Props {}
 
-const CareReceiverPage: React.SFC<Props> = () => (
-  <AppTemplate
-    header={<Header right={<AddButton>追加</AddButton>} title="本人情報" />}>
-    <CareReceiverList
-      careReceivers={[
-        {
-          id: 0,
-          name: '左藤太郎',
-          birth: '76歳 1941年1月15日生',
-          folders: [{ id: 0, name: 'Title1' }, { id: 1, name: 'Title2' }],
-        },
-        {
-          id: 1,
-          name: '左藤二郎',
-          birth: '76歳 1941年2月13日生',
-          folders: [{ id: 0, name: 'Title1' }, { id: 1, name: 'Title2' }],
-        },
-      ]}
-    />
-  </AppTemplate>
-);
+export interface State {
+  modalIsOpen: boolean;
+}
+
+class CareReceiverPage extends React.Component<Props, State> {
+  state = {
+    modalIsOpen: false,
+  };
+
+  toggleModal = () => {
+    this.setState({ modalIsOpen: !this.state.modalIsOpen });
+  };
+
+  render() {
+    return (
+      <div>
+        <AppTemplate
+          header={
+            <Header
+              right={<AddButton onClick={this.toggleModal}>追加</AddButton>}
+              title="本人情報"
+            />
+          }>
+          <CareReceiverList
+            careReceivers={[
+              {
+                id: 0,
+                name: '左藤太郎',
+                birth: '76歳 1941年1月15日生',
+                folders: [{ id: 0, name: 'Title1' }, { id: 1, name: 'Title2' }],
+              },
+              {
+                id: 1,
+                name: '左藤二郎',
+                birth: '76歳 1941年2月13日生',
+                folders: [{ id: 0, name: 'Title1' }, { id: 1, name: 'Title2' }],
+              },
+            ]}
+          />
+        </AppTemplate>
+        <ModalTemplate
+          header={
+            <Header
+              left={<a onClick={this.toggleModal}>キャンセル</a>}
+              right={<AddButton>{'追加'}</AddButton>}
+              title="タイトルの追加"
+            />
+          }
+          isOpen={this.state.modalIsOpen}>
+          Main
+        </ModalTemplate>
+      </div>
+    );
+  }
+}
 
 export default CareReceiverPage;
