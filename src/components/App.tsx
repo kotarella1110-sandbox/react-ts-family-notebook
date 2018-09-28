eb819bbab4af10ea5b90301344355a52e9490a2d
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-28 18:17:10 +0900

    Fixed App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 import { ThemeProvider } from 'styled';
 import globalStyle from './globalStyle';
 import theme from './theme';
+import CareReceiverPage from 'containers/CareReceiverPage';
 
 globalStyle(theme);
 
-const App: React.SFC<{}> = () => <ThemeProvider theme={theme} />;
+const App: React.SFC = () => (
+  <ThemeProvider theme={theme}>
+    <CareReceiverPage />
+  </ThemeProvider>
+);
 
 export default App;
