8abfeee881c4ff55f6aed8567b329f2bc98c0f2d
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-28 18:36:14 +0900

    Revert "Fixed App component"
    This reverts commit eb819bbab4af10ea5b90301344355a52e9490a2d.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,9 @@ import * as React from 'react';
 import { ThemeProvider } from 'styled';
 import globalStyle from './globalStyle';
 import theme from './theme';
-import CareReceiverPage from 'containers/CareReceiverPage';
 
 globalStyle(theme);
 
-const App: React.SFC = () => (
-  <ThemeProvider theme={theme}>
-    <CareReceiverPage />
-  </ThemeProvider>
-);
+const App: React.SFC<{}> = () => <ThemeProvider theme={theme} />;
 
 export default App;
