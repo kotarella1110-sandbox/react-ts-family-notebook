4c060fc45677de250903b150a5e3db983c327f52
Kotaro Sugawara <kotarella1110@gmail.com>
2018-10-05 13:36:40 +0900

    Added CareReceiverInfoDetailPage container

---
+++ src/containers/CareReceiverInfoDetailPage.tsx
@@ -0,0 +1,34 @@
+import { Dispatch, bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Action } from 'typescript-fsa';
+import { mayBeStubbed } from 'react-stubber';
+import { State, CareReceiverEntities, FolderEntities } from 'models';
+import { getCareReceiver, getFolder } from 'store/selectors';
+import * as actions from 'store/actions';
+import CareReceiverInfoDetailPage from 'components/pages/CareReceiverInfoDetailPage';
+
+export interface OwnProps {
+  careReceiverId: CareReceiverEntities['id'];
+  folderId: FolderEntities['id'];
+}
+
+const mapStateToProps = (state: State, ownProps: OwnProps) => ({
+  careReceiver: getCareReceiver(state, ownProps),
+  folder: getFolder(state, ownProps),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<Action<any>>) =>
+  bindActionCreators<any, any>(
+    {
+      fetchCareReceivers: actions.fetchCareReceivers.started,
+      addFolderContent: actions.addFolderContent.started,
+    },
+    dispatch
+  );
+
+const CareReceiverInfoDetailPageContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CareReceiverInfoDetailPage);
+
+export default mayBeStubbed(CareReceiverInfoDetailPageContainer);
